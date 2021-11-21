be8c6f60ccaa12a820d5a3f4982b6c4e49d6f30f
danielvogler <davogler@ethz.ch>
2021-11-21 18:00:20 +0100

    data download: add config file

--- data_scripts/download_NEM_data.py
+++ data_scripts/download_NEM_data.py
@@ -10,22 +10,26 @@ url_address = "https://aemo.com.au/aemo/data/nem/priceanddemand/PRICE_AND_DEMAND
 import os
 import sys
 from pathlib import Path
+import yaml
 
 script_dir = Path( os.path.split( os.path.abspath( sys.argv[0] ) )[0] )
-data_dir = script_dir / '../data/'
+config_file = script_dir / '../config/config.yaml'
+
+with open(config_file, "r") as f:
+  cfg = yaml.safe_load(f)
+
+data_dir = script_dir / cfg['data']['data_path']
 
 os.makedirs(data_dir, exist_ok=True) 
 
-start_year = 2010
-end_year = 2020
+start_year = cfg['data']['start_year']
+end_year = cfg['data']['end_year']
 
-states = {"NSW","VIC","QLD","SA","TAS"}
+states = cfg['data']['states']
 
-url_prefix = "https://aemo.com.au/aemo/data/nem/priceanddemand/PRICE_AND_DEMAND"
-
-
+### load data for states and months
 for state in states:
   for year in range(start_year, end_year+1):
     for month in range(1,13): 
-      url_address = url_prefix +  "_"  + str(year) +  str(month).zfill(2) +"_" + state + "1.csv"
+      url_address = cfg['data']['url_prefix'] +  "_"  + str(year) +  str(month).zfill(2) +"_" + state + "1.csv"
       os.system( "wget {} -P {}".format(url_address, data_dir) )
